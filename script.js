7adc7fc8315868bb72604f20f9d7478f04115b4d
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-07-01 13:30:39 +0300

    _.extend, _.bind methods

--- script.js
+++ script.js
@@ -15,27 +15,33 @@ function getCoords(data) {
   var weatherApi = "http://api.openweathermap.org/data/2.5/forecast?APPID=" + 
                     appId + "&callback=?";
 
-function getRefreshedData(iss, weather, astronauts) {
+function ISSFlyover() {
+  this.refreshData();
+  _.bindAll(this, 'refreshData', 'getRefreshedData');
+  $('#refresh').on('click', this.refreshData);
+
+}
+
+_.extend(ISSFlyover.prototype, { 
+  getRefreshedData: function(iss, weather, astronauts) {
     console.log('ISS', iss);
     console.log('WEATHER', weather);
 
-  var atTheISS = _.pluck(_.where(astronauts.people, {craft:"ISS"}), "name");
-  // atTheISS = _.map(atTheIss, function (person) {
-  //   return person.name;
-  // }); 
-  //atTheISS = _.pluck(atTheISS, "name");
-  $('#astronauts').text(atTheISS.join(", "));
+    var atTheISS = _.pluck(_.where(astronauts.people, {
+      craft: "ISS"
+    }), "name");
+    $('#astronauts').text(atTheISS.join(", "));
 
 
-function outputFlyover(flyover, i) {
-      $('#flyovers').append('<div>Flyover at ' + 
-        flyover.risetime +': '+flyover.weatherDescription+ '</div>');
+    function outputFlyover(flyover, i) {
+      $('#flyovers').append('<div>Flyover at ' +
+        flyover.risetime + ': ' + flyover.weatherDescription + '</div>');
     }
 
-function processFlyoverData(flyover) {
-  var weatherAtFlyover = _.find(weather.list, function (w){
-    return w.dt <= flyover.risetime && w.dt + 60*60*3 > flyover.risetime;
-  })
+    function processFlyoverData(flyover) {
+      var weatherAtFlyover = _.find(weather.list, function(w) {
+        return w.dt <= flyover.risetime && w.dt + 60 * 60 * 3 > flyover.risetime;
+      });
       return {
         clouds: weatherAtFlyover && weatherAtFlyover.clouds.all,
         hasWeather: !_.isUndefined(weatherAtFlyover),
@@ -45,69 +51,46 @@ function processFlyoverData(flyover) {
       };
     }
 
-function getDay(flyover){
-  return flyover.risetime.toDateString();
-}
+    function getDay(flyover) {
+      return flyover.risetime.toDateString();
+    }
 
-  var flyovers = _.map(iss.response, processFlyoverData);
-
-
-    // var flyoversWithWeather = _.filter(flyovers, function (flyover){
-    //   return flyover.hasWeather;
-    // });
-
-  var flyoversWithWeather = _.where(flyovers, {hasWeather: true});
-
-  var flyoversGrouped = _.groupBy(flyoversWithWeather, getDay);
-
-  //var days = _.keys(flyoversGrouped);
+    var flyovers = _.map(iss.response, processFlyoverData);
+    var flyoversWithWeather = _.where(flyovers, {
+      hasWeather: true
+    });
+    var flyoversGrouped = _.groupBy(flyoversWithWeather, getDay);
 
 
-  _.each(flyoversGrouped, function (flyoversForDay, day) {
-    $('#flyovers').append('<h2>'+day+'</h2>');
-    var flyoversForDay = flyoversGrouped[day];
-    //flyoversForDay = _.sortBy(flyoversForDay, 'clouds');
-    _.each(flyoversForDay, outputFlyover);
-  });
+    _.each(flyoversGrouped, function(flyoversForDay, day) {
+      $('#flyovers').append('<h2>' + day + '</h2>');
+      var flyoversForDay = flyoversGrouped[day];
+      _.each(flyoversForDay, outputFlyover);
+    });
 
-  var summary = _.countBy(flyoversWithWeather, 'weatherDescription');
-  $('#summary').html('');
-  _.each(summary, function (count, condition){
-    $('#summary').append('<div><b>'+condition+'</b>:'+count+'</div>');
-  });
+    var summary = _.countBy(flyoversWithWeather, 'weatherDescription');
+    $('#summary').html('');
+    _.each(summary, function(count, condition) {
+      $('#summary').append('<div><b>' + condition + '</b>:' + count + '</div>');
+    });
 
-  // _.each(days, function (day) {
-  //   $('#flyovers').append('<h2>'+day+'</h2>');
-  //   var flyoversForDay = flyoversGrouped[day];
-  //   flyoversForDay = _.sortBy(flyoversForDay, 'clouds');
-  //   _.each(flyoversForDay, outputFlyover);
-  // });
+  },
 
-
-
-   // _.each(flyoversWithWeather, outputFlyover);
-
-    //_.each(flyovers, outputFlyover);
-
-    //_.each(iss.response, outputFlyover);
-
-    // for(var i=0; i< iss.response.length; i++ ){
-    //   console.log(iss.response[i]);
-    // }
-
-  }
-
-function refreshData() {
-    jQuery.getJSON(astro, function (astronauts){ 
-    jQuery.getJSON(apiOn, _.extend({n: 100}, location), function (iss) {
-      jQuery.getJSON(weatherApi, location, function (weather) {
-        getRefreshedData(iss, weather, astronauts);
+  refreshData: function() {
+    var me = this;
+    jQuery.getJSON(astro, function(astronauts) {
+      jQuery.getJSON(apiOn, _.extend({
+        n: 100
+      }, location), function(iss) {
+        jQuery.getJSON(weatherApi, location, function(weather) {
+          me.getRefreshedData(iss, weather, astronauts);
+        });
       });
     });
-  });
-  };
-  refreshData();
-  $('#refresh').on('click', refreshData);
+  }
+});
+var ISSFlyover = new ISSFlyover();
+  
 
 }
 
